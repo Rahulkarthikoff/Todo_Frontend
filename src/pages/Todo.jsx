71dd84b1b9f4202350bdb4118c359a8138101f62
Rahulkarthikoff <rahulkarthikoff@gmail.com>
2025-05-07 09:58:34 +0530

    first commit

---
+++ src/pages/Todo.jsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+const Todo = () => {
+    const [todo, setTodo] = useState("");
+    const [db, setDb] = useState([])
+    function post() {
+        axios.post("http://localhost:5000/api/post", { todo })
+            .then(() => {
+                alert("data has been stored")
+                get();
+                setTodo("")
+            })
+            .catch((err) => {
+                console.error(err);
+
+            })
+    }
+    function get() {
+        axios.get("http://localhost:5000/api/get")
+            .then((res) => {
+                setDb(res.data)
+            })
+            .catch((err) => {
+                console.error(err)
+            })
+    }
+    // console.log(db);
+
+    function update(data, id) {
+        axios.put(`http://localhost:5000/api/update/${id}`, { todo: data })
+            .then(() => {
+                get();
+            })
+            .catch((err) => {
+                console.error(err);
+
+            })
+    }
+
+    function del(id) {
+        axios.delete(`http://localhost:5000/api/delete/${id}`)
+            .then(() => {
+                get();
+            })
+            .catch((err) => {
+                console.error(err);
+            })
+
+    }
+
+    useEffect(()=>{
+        get()
+    },[])
+
+
+    return (
+        <div style={{ margin: '100px' }}>
+            <label htmlFor="todo">Todo</label>
+            <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)} />
+            <button onClick={post}>Post</button>
+            <ul>
+                {
+                    db.map((it) => (
+                        <li key={it._id}>{it.todo}<button onClick={() => {
+                            const data = prompt("enter the data").trim();
+                            update(data, it._id)
+                        }}>up</button>            <button onClick={() => del(it._id)}>del</button>    </li>
+                    ))
+                }
+            </ul>
+        </div>
+    )
+}
+
+export default Todo
